bf904bd2dcc193b4c4ccc8794cf851c57ff6d1ea
David Porras <davidpu@gmai.com>
2023-06-08 19:06:48 -0600

    Adding Swagger UI

--- src/main/java/com/incofer/demo/rest/TrainResource.java
+++ src/main/java/com/incofer/demo/rest/TrainResource.java
@@ -4,8 +4,9 @@
 package com.incofer.demo.rest;
 
 import com.incofer.demo.model.Train;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,10 +28,11 @@ public interface TrainResource
      */
     @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Request processed successfully"),
-            @ApiResponse(responseCode = "400", description = "Bad request"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiOperation("Example API endpoint")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "Not Found")
     })
     Train searchTrain(@RequestParam long id) throws Exception;
 }
